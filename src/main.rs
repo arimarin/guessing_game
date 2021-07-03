9ca1082671512c11a185b5a25d1347353a64cf7e
Ari Marin <ari.marin@oscar.fi>
2021-07-03 18:38:34 +0300

    Comments

--- src/main.rs
+++ src/main.rs
@@ -9,20 +9,23 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1..101);
 
     loop {
+        // Wait for input
         print!("Please input your guess (`quit` or `exit` to exit): ");
         io::stdout()
             .flush()
             .expect("Failed to flush stdout!");
 
+        // Get input
         let mut guess = String::new();
-
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line!");
 
+        // Try to parse given string to integer
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
+                // With `quit` or `exit`, break the loop -> exit, any other continues to next round
                 match guess.to_lowercase().trim() {
                     "quit" | "exit" => {
                         println!("Received {}, exiting program...", guess.trim());
@@ -36,8 +39,8 @@ fn main() {
             },
         };
 
+        // Guessed value and was it less, greater or equal. When equal -> exit
         println!("You guessed: {}", guess);
-
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
