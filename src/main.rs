8ae68906183bc95e4286dc7b0a0ead694d215019
Ari Marin <ari.marin@oscar.fi>
2021-07-03 16:53:51 +0300

    expect to flush and code formatting

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,15 @@ fn main() {
     println!("Guess the number!");
 
     print!("Please input your guess: ");
-    io::stdout().flush().unwrap();
+    io::stdout()
+        .flush()
+        .expect("Failed to flush stdout!66");
 
     let mut guess = String::new();
 
     io::stdin()
         .read_line(&mut guess)
-        .expect("Failed to read line");
+        .expect("Failed to read line!");
 
     println!("You guessed: {}", guess)
 }
